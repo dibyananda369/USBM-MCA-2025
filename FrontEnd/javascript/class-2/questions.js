7b3acd33f0ca3f1e83245a52e194627acb7f5930
dibyananda <dibyanandasubudhi369@gmail.com>
2024-10-07 22:50:25 +0530

    Questions

---
+++ FrontEnd/javascript/class-2/questions.js
@@ -0,0 +1,15 @@
+//Question-1
+let foodprice=100;
+let drinkprice=40;
+let totalPrice=foodprice+drinkprice;
+let tip=totalPrice*10/100;
+console.log(`final amount :${totalPrice+tip}`);
+//question-2
+let originalPrice=500;
+ let discountAmount=originalPrice*20/100;
+ console.log(`final amount: ${originalPrice-discountAmount}`);
+ //question-3
+ let productPrice=600;
+ let salesTaxRate=0.08;
+ let tax=productPrice*salesTaxRate;
+ console.log(`total price:${productPrice+tax}`);
